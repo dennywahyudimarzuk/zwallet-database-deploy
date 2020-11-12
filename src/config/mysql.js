221a1120df175e174c5ad6926912204a4547224b
dennywahyudimarzuk <dennywm66@gmail.com>
2020-11-12 16:40:55 +0700

    deploy lagi

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -1,19 +1,34 @@
-const mysql = require('mysql')
-require('dotenv').config()
+// const mysql = require('mysql')
+// require('dotenv').config()
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-})
+// const connection = mysql.create({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// })
 
-connection.connect(err => {
-    if(!err) {
-        console.log(`MySQL Conected`)
-    } else {
-        console.log(err)
-    }
-})
+// connection.connect(err => {
+//     if(!err) {
+//         console.log(`MySQL Conected`)
+//     } else {
+//         console.log(err)
+//     }
+// })
 
-module.exports = connection
+// module.exports = connection
+
+const mysql = require("mysql");
+const conn = mysql.createPool({
+  connectionLimit: process.env.LIMIT,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+conn.getConnection((err) => {
+  if (!err) {
+    console.log("Success connect to DB");
+  }
+});
+module.exports = conn;
